b545da6c0b1481eba68141316320a8bc4c59d0ce
Mikita Stefanishin <stefanishin92@gmail.com>
2020-10-03 17:25:07 +0300

    addSolveTask1

--- HW1/ConsoleAppTask1/ConsoleAppTask1/Program.cs
+++ HW1/ConsoleAppTask1/ConsoleAppTask1/Program.cs
@@ -5,9 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 /*
 Задание 1.
-Написать программу, которая считывает символы с клавиатуры, пока не
-будет введена точка. Программа должна сосчитать количество введенных
-пользователем пробелов.
+Написать программу, которая считывает символы с клавиатуры, пока не будет введена точка. 
+Программа должна сосчитать количество введенных пользователем пробелов.
  */
 namespace ConsoleAppTask1
 {
@@ -15,6 +14,28 @@ namespace ConsoleAppTask1
     {
         static void Main(string[] args)
         {
+            int spaceSymbol = 0;
+            char symbol;
+            Console.WriteLine("Программа для подсчета пробелов! ");
+            do
+            {
+                Console.WriteLine("Введите символ");
+                Console.WriteLine("Нажмите кнопку точку,  для выхода из циклов");
+                symbol = Console.ReadKey().KeyChar;                
+                Console.Clear();
+
+                Console.WriteLine(symbol);
+                if (symbol == ' ')
+                {
+                    Console.WriteLine("Пробел");
+                    spaceSymbol++;
+                    Console.WriteLine($"Количество введенных пробелов = {spaceSymbol}");
+                }
+                //if (symbol.KeyChar != '.')
+                //{
+                //    break;
+                //}
+            } while (symbol != '.');
         }
     }
 }
